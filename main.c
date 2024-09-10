012347fb8d527e34c41fa0582d29757e701087e2
GBarbosa <andradegui2006@gmail.com>
2024-09-10 20:21:20 -0300

    versao 1 feita

--- main.c
+++ main.c
@@ -2,5 +2,33 @@
 
 int main()
 {
-    printf("Teste de codigo");
+    int n, acertos;
+    char gabarito[n], resposta[n];
+
+    printf("Quantas questoes? ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        printf("Digite a reposta da questao %d: ", i);
+        scanf("%s", &resposta[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("*");
+    }
+    
+    for(int i = 1; i <= n; i++)
+    {
+        printf("Digite o gabarito da questao %d: ", i);
+        scanf("%s", &gabarito[i]);
+    }
+    for (int i = 1; i < n; i++)
+    {
+        if(resposta[i] == gabarito[i])
+        {
+            acertos++;
+        }
+    }
+    printf("Acertou %d questoes", acertos);
 }
